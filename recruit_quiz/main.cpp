15556213bbacbe2e0ad4c69a94a4bb089ddc1a13
alt5656-cmd <alt5656@gmail.com>
2026-04-21 11:43:20 +0900

    構造体の追加により、問題を複数に

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+struct Question
+{
+	string q;
+	int a;
+};
+
 int main()
 {
+	Question questions[] = {
+		{"13x(-5)",13 * -5},
+		{"(-21)÷(-3)", -21 / -3},
+		{"7-(4÷2)÷2",7-(4/2)/2},
+	};
+
 	cout << "[リクルート試験対策クイズ]\n";
-	cout << "13x(-5)の答えは?\n";
 
-	int answer;
-	cin >> answer;
-	if (answer == 13 * -5)
-	{
-		cout << "正解!\n";
-	}
-	else
-	{
-		cout << "間違い!正解は" << 13 * -5 << "\n";
-	}
+	for (const auto& e : questions) {
+		cout << e.q << "の答えは?\n";
 
+		int answer;
+		cin >> answer;
+		if (answer == e.a)
+		{
+			cout << "正解!\n";
+		}
+		else
+		{
+			cout << "間違い!正解は" << e.a << "\n";
+		}
+	}
 }
