0740f570eeee4690f02a9f161ac768a91a61fcd7
alt5656-cmd <alt5656@gmail.com>
2026-04-28 11:18:57 +0900

    サイコロ

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -11,6 +11,19 @@ struct Question
 	string a;
 };
 
+//最大公約数を求める
+int gcd(int a, int b)
+{
+	while (b)
+	{
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+
 int main()
 {
 	vector<Question> questions(3);
@@ -46,11 +59,21 @@ int main()
 	questions.push_back({ "底面積の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3とする。Xの値を求めよ。" ,
 		to_string(x * x * y / 3)});
 
-	x = uniform_int_distribution<>(1, 5)(rand) * 3;
+	x = uniform_int_distribution<>(1, 3)(rand) * 3;
 	questions.push_back({
 		"半径" + to_string(x) + "cmの球がある。\n" + "この球の体積をXπcm^3とする。Xの値を求めよ。"
-		,to_string(x * x * x * 4/3)
+		,to_string(4 * x * x * x /3 )
 		});
+
+	//サイコロ
+	x = uniform_int_distribution<>(1, 5)(rand);
+	y = uniform_int_distribution<>(1, 6 - x)(rand);
+	z = gcd(y + 1, 6);
+	questions.push_back({
+		"サイコロを1個振って、" + to_string(x) + "から" + to_string(x + y) + "がでる確率を求めよ。",
+		to_string((y + 1)/z) + "/" + to_string(6/z)
+	});
+
 
 	cout << "[リクルート試験対策クイズ]\n";
 
