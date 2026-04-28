438445dd9b6fa6ecfa1e8d4b12bb63109ee48dac
alt5656-cmd <alt5656@gmail.com>
2026-04-28 10:51:35 +0900

    円錐

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -41,6 +41,11 @@ int main()
 		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ", x
 		});
 
+	x = uniform_int_distribution<>(1, 10)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand)*3;
+	questions.push_back({ "底面積の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3とする" ,
+		x * x * y / 3});
+
 
 
 	cout << "[リクルート試験対策クイズ]\n";
