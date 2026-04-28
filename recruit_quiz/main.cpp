99bc6dda17690f63554c246277230e8c0ccb2203
alt5656-cmd <alt5656@gmail.com>
2026-04-28 11:05:47 +0900

    答えをstringに変更

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 struct Question
 {
 	string q;
-	int a;
+	string a;
 };
 
 int main()
@@ -21,35 +21,35 @@ int main()
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 30)(rand);
 	questions[0].q = to_string(x) + "x" + to_string(y) + "の答えは?";
-	questions[0].a = x * y;
+	questions[0].a = to_string(x * y);
 
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 30)(rand);
 	questions[1].q = to_string(x*y) + "÷" + to_string(y) + "の答えは?";
-	questions[1].a = x;
+	questions[1].a = to_string(x);
 
 	x = uniform_int_distribution<>(1, 100)(rand);
 	y = uniform_int_distribution<>(1, 10)(rand);
 	int z = uniform_int_distribution<>(1, 10)(rand);
 	int w = uniform_int_distribution<>(1, 10)(rand);
 	questions[2].q = to_string(x) + "-(" + to_string(y*w) + "+" + to_string(z*w) + ")÷" + to_string(w) + "の答えは?";
-	questions[2].a = x - (y + z);
+	questions[2].a = to_string(x - (y + z));
 
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand) * 2;
 	questions.push_back({
-		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ", x
+		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ", to_string(x)
 		});
 
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand)*3;
 	questions.push_back({ "底面積の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3とする。Xの値を求めよ。" ,
-		x * x * y / 3});
+		to_string(x * x * y / 3)});
 
 	x = uniform_int_distribution<>(1, 5)(rand) * 3;
 	questions.push_back({
 		"半径" + to_string(x) + "cmの球がある。\n" + "この球の体積をXπcm^3とする。Xの値を求めよ。"
-		,x * x * x * 4/3
+		,to_string(x * x * x * 4/3)
 		});
 
 	cout << "[リクルート試験対策クイズ]\n";
@@ -57,7 +57,7 @@ int main()
 	for (const auto& e : questions) {
 		cout << e.q << "\n";
 
-		int answer;
+		string answer;
 		cin >> answer;
 		if (answer == e.a)
 		{
