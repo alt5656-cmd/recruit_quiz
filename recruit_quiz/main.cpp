2cb242c40ffcb326d85e0c540352848764278f39
alt5656-cmd <alt5656@gmail.com>
2026-04-28 10:45:47 +0900

    文末変更

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -20,19 +20,19 @@ int main()
 
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 30)(rand);
-	questions[0].q = to_string(x) + "x" + to_string(y);
+	questions[0].q = to_string(x) + "x" + to_string(y) + "の答えは?";
 	questions[0].a = x * y;
 
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 30)(rand);
-	questions[1].q = to_string(x*y) + "÷" + to_string(y);
+	questions[1].q = to_string(x*y) + "÷" + to_string(y) + "の答えは?";
 	questions[1].a = x;
 
 	x = uniform_int_distribution<>(1, 100)(rand);
 	y = uniform_int_distribution<>(1, 10)(rand);
 	int z = uniform_int_distribution<>(1, 10)(rand);
 	int w = uniform_int_distribution<>(1, 10)(rand);
-	questions[2].q = to_string(x) + "-(" + to_string(y*w) + "+" + to_string(z*w) + ")÷" + to_string(w);
+	questions[2].q = to_string(x) + "-(" + to_string(y*w) + "+" + to_string(z*w) + ")÷" + to_string(w) + "の答えは?";
 	questions[2].a = x - (y + z);
 
 	x = uniform_int_distribution<>(1, 10)(rand);
@@ -46,7 +46,7 @@ int main()
 	cout << "[リクルート試験対策クイズ]\n";
 
 	for (const auto& e : questions) {
-		cout << e.q << "の答えは?\n";
+		cout << e.q << "\n";
 
 		int answer;
 		cin >> answer;
